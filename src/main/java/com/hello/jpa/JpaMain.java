92e6c8d8bd4814f8eb647decb91968e95e450205
ym1085 <youngmin1085@gmail.com>
2022-01-22 15:17:38 +0900

    FEAT: 플러시가 발생하면 무슨일이 생기는가?
    1. em.flush() 강제 호출

--- src/main/java/com/hello/jpa/JpaMain.java
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -17,19 +17,11 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
-
-            // 영속 상태, 영속성 컨텍스트 안에 member 객체가 들어가 관리된다
-            // BEFORE, AFTER 사이에 쿼리 질의문이 나오지 않는 상황
-            System.out.println("==== BEFORE ====");
+            Member member = new Member(200L, "member200");
             em.persist(member);
-//            em.detach(member); // 영속 상태 제거
-            System.out.println("==== AFTER ====");
+            em.flush(); // 강제 호출
 
-            // tx.commit 시 영속성 컨텍스트의 내용이 DB에 날라간다
+            System.out.println("=========================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();

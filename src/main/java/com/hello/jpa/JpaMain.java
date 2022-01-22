ac2c5301799706385967fc1d579f742ffe9a129d
ym1085 <youngmin1085@gmail.com>
2022-01-22 15:36:43 +0900

    FEAT: 준영속 상태
    1. em.flush()
    2. em.clear()
    3. em.close()

--- src/main/java/com/hello/jpa/JpaMain.java
+++ src/main/java/com/hello/jpa/JpaMain.java
@@ -17,9 +17,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member = new Member(200L, "member200");
-            em.persist(member);
-            em.flush(); // 강제 호출
+            Member member = em.find(Member.class, 150L);
+            member.setName("AAAAA");
+
+//            em.detach(member); // 준 영속 상태 (JPA에서 관리가 되지 않는다)
+            em.clear(); // 영속성 컨텍스트 (1차 캐시)를 전부 제거한다
+//            em.close(); // 영속성 컨텍스트를 닫는다, 데이터 변경사항이 적용되지 않는다
+
+            // 영속성 컨텍스트가 전부 제거되면, 1차 캐시에 값이 없기 때문에 쿼리가 두번 나간다
+            Member member2 = em.find(Member.class, 150L);
 
             System.out.println("=========================");
             tx.commit();
